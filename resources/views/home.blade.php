9d69100022d300114a7febc97bbd333d19e68641
Ferdinando <ferdi.dagostino@gmail.com>
2020-06-30 16:58:35 +0200

    sistemo la navbar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,48 @@
 @extends('layouts.app')
 
 @section('content')
+<section id="jumbotron">
     <div class="container">
         <div class="row">
-        AAAAAAAAAAA
+            <div class="col-6">
+                <h1 class="">
+                    DIVENTA <strong>SVILUPPATORE WEB</strong>
+                </h1>
+                <p>Trasformiamo la tua passione in una carriera. Se non trovi lavoro, ti rimborsiamo.</p>
+                <ul class="">
+                    <li class="paragraph"> <strong>6 mesi</strong> di corso intensivo online in diretta</li>
+                    <li class="paragraph"><strong>Nessuna competenza</strong> di programmazione richiesta</li>
+                    <li class="paragraph">Siamo certi del tuo successo, altrimenti <strong>ti rimborsiamo</strong></li>
+                </ul>
+            </div>
+            <div class="col-6">
+                <img src="{{ asset('images/pc-black-gif.gif') }}" alt="">
+            </div>
         </div>
     </div>
+</section>
+<section id="data">
+    <div class="container">
+        <div class="row">
+            <div class="col-4 text-center">
+                <h3>98%</h3>
+                <p>La percentuale dei nostri studenti che ora lavora come web developer, oltre la metà a tempo indeterminato.</p>
+            </div>
+            <div class="col-4 text-center">
+                <h3>€ 23.000</h3>
+                <p>Lo stipendio medio lordo di partenza degli studenti assunti dalle nostre aziende partner.</p>
+            </div>
+            <div class="col-4 text-center">
+                <h3>#1</h3>
+                <p>Siamo il primo istituto online in Italia per indice di gradimento e risultati conseguiti.</p>
+            </div>
+        </div>
+
+    </div>
+
+</section>
+
+
 @endsection
 
 @section('page-title', 'Il corso per diventare web developer e trovare un nuovo lavoro')
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,30 +1,33 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="{{ route('home') }}">
-      <img src="{{ asset('images/logo.png') }}" alt="Boolean logo">
-  </a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
+    <a class="navbar-brand" href="{{ route('home') }}">
+        <img src="{{ asset('images/logo.png') }}" alt="Boolean logo">
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item">
-        <a class="nav-link" href="{{ route('home') }}">HOME</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="">CORSO</a>
-      </li>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="">DOPO IL CORSO</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="">LEZIONE GRATUITA</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="">ASSUMI I NOSTRI STUDENTI</a>
-      </li>
-      {{-- <li class="nav-item dropdown">
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('home') }}">HOME</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="">CORSO</a>
+            </li>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="">DOPO IL CORSO</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="">LEZIONE GRATUITA</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="">ASSUMI I NOSTRI STUDENTI</a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="myButton">CANDIDATI ORA</a>
+            </li>
+            {{-- <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           Dropdown
         </a>
@@ -35,6 +38,6 @@
           <a class="dropdown-item" href="#">Something else here</a>
         </div>
       </li> --}}
-    </ul>
-  </div>
+        </ul>
+    </div>
 </nav>
